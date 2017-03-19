3d020c04dba35ef27035426ef6cf46014421d9ea
David Molnar <dmolnar@qualityunit.com>
2017-03-19 19:42:57 +0100

    add possibility to reacquire owned queue lock

--- lib/ResqueSerial/QueueLock.php
+++ lib/ResqueSerial/QueueLock.php
@@ -7,7 +7,19 @@ namespace ResqueSerial;
 class QueueLock {
 
     const RELEASE_SCRIPT = <<<LUA
-if redis.call('get',KEYS[1]) == ARGV[1] then return redis.call('del',KEYS[1]) else return 0 end
+if redis.call('get', KEYS[1]) == ARGV[1] then
+    return redis.call('del', KEYS[1])
+else
+    return 0
+end
+LUA;
+    const ACQUIRE_SCRIPT = <<<LUA
+local val = redis.call('get', KEYS[1])
+if not val or val == ARGV[1] then
+    return redis.call('set', KEYS[1], ARGV[1], 'PX', ARGV[2])
+else
+    return 0
+end
 LUA;
 
     private $lockKey;
@@ -30,11 +42,15 @@ LUA;
         return \Resque::redis()->get(Key::queueLock($queue)) !== false;
     }
 
-    public function acquire() {
-        $response = \Resque::redis()->set($this->lockKey, $this->lockValue, [
-                0 => 'nx', // only if it does not exist yet
-                'px' => $this->time // rexpire time, in millis
-        ]);
+    public function acquire($time = null) {
+        if ($time == null) {
+            $time = $this->time;
+        }
+        $response = \Resque::redis()->eval(
+                self::ACQUIRE_SCRIPT,
+                [\Resque_Redis::getPrefix() . $this->lockKey],
+                [$this->lockValue, $time]
+        );
         if (!$response) {
             return false;
         }
