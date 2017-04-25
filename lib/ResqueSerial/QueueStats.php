e94d7321608d088651b138c934940927c1cf4bea
David Molnar <dmolnar@qualityunit.com>
2017-04-25 14:43:29 +0200

    separate queue stats by hostname

--- lib/ResqueSerial/QueueStats.php
+++ lib/ResqueSerial/QueueStats.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace ResqueSerial;
+
 use Resque;
 
 /**
@@ -12,26 +13,14 @@ use Resque;
  */
 class QueueStats {
     /**
-     * Get the value of the supplied statistic counter for the specified statistic.
+     * Delete a statistic with the given name.
      *
-     * @param string $stat The name of the statistic to get the stats for.
-     *
-     * @return mixed Value of the statistic.
-     */
-    public static function get($queue, $stat) {
-        return (int)Resque::redis()->get("queuestat:$stat:$queue");
-    }
-
-    /**
-     * Increment the value of the specified statistic by a certain amount (default is 1)
-     *
-     * @param string $stat The name of the statistic to increment.
-     * @param int $by The amount to increment the statistic by.
+     * @param string $stat The name of the statistic to delete.
      *
      * @return boolean True if successful, false if not.
      */
-    public static function incr($queue, $stat, $by = 1) {
-        return (bool)Resque::redis()->incrby("queuestat:$stat:$queue", $by);
+    public static function clear($queue, $stat) {
+        return (bool)Resque::redis()->del(self::key($queue, $stat));
     }
 
     /**
@@ -43,17 +32,33 @@ class QueueStats {
      * @return boolean True if successful, false if not.
      */
     public static function decr($queue, $stat, $by = 1) {
-        return (bool)Resque::redis()->decrby("queuestat:$stat:$queue", $by);
+        return (bool)Resque::redis()->decrby(self::key($queue, $stat), $by);
     }
 
     /**
-     * Delete a statistic with the given name.
+     * Get the value of the supplied statistic counter for the specified statistic.
      *
-     * @param string $stat The name of the statistic to delete.
+     * @param string $stat The name of the statistic to get the stats for.
+     *
+     * @return mixed Value of the statistic.
+     */
+    public static function get($queue, $stat) {
+        return (int)Resque::redis()->get(self::key($queue, $stat));
+    }
+
+    /**
+     * Increment the value of the specified statistic by a certain amount (default is 1)
+     *
+     * @param string $stat The name of the statistic to increment.
+     * @param int $by The amount to increment the statistic by.
      *
      * @return boolean True if successful, false if not.
      */
-    public static function clear($queue, $stat) {
-        return (bool)Resque::redis()->del("queuestat:$stat:$queue");
+    public static function incr($queue, $stat, $by = 1) {
+        return (bool)Resque::redis()->incrby(self::key($queue, $stat), $by);
+    }
+
+    private static function key($queue, $stat) {
+        return 'queuestat:' . gethostname() . ":$stat:$queue";
     }
 }
