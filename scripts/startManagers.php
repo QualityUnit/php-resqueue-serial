7102ffc704f50a4b8e3e7f3697267f323ca055f1
Jakub Zernovic <jzernovic@qualityunit.com>
2017-08-07 14:03:11 +0200

    read global config file path from command line

--- scripts/startManagers.php
+++ scripts/startManagers.php
@@ -1,9 +1,16 @@
 #!/usr/bin/env php
 <?php
 
+require_once 'bootstrap.php';
+
+use ResqueSerial\Init\GlobalConfig;
 use ResqueSerial\Redis;
 
-require_once 'bootstrap.php';
+if($argc !== 2 || !file_exists($argv[1])) {
+    throw new Exception('Expected config file as command line parameter');
+}
+
+GlobalConfig::$PATH = $argv[1];
 
 Redis::prefix(ResqueSerial::VERSION);
 
