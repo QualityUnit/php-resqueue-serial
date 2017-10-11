f9afdbd321d274a647f876c7368bd1473f4cbc0a
Jakub Zernovic <jzernovic@qualityunit.com>
2017-10-11 15:50:03 +0200

    add comments for RescheduleException

--- lib/Resque/Api/RescheduleException.php
+++ lib/Resque/Api/RescheduleException.php
@@ -11,13 +11,20 @@ class RescheduleException extends Exception {
     /** @var int */
     private $delay;
 
+    /**
+     * RescheduleException constructor.
+     * @param int $delay in seconds
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
     public function __construct($delay = 0, $message = "", $code = 0, Throwable $previous = null) {
         parent::__construct($message, $code, $previous);
         $this->delay = $delay;
     }
 
     /**
-     * @return int
+     * @return int (in seconds)
      */
     public function getDelay() {
         return $this->delay;
